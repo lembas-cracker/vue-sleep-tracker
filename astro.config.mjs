0665730664120b21ad539619ec13b04d60dbba9f
Ksenia Agalakova <agalaksu111@gmail.com>
2026-09-18 19:25:15 +0300

    Added Layout and 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [vue()]
+  integrations: [vue(), icon()],
 });
